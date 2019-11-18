38fbae58e3ec808d0b57aea5ff25535b2bd6270d
Shamim <35076893+sphp@users.noreply.github.com>
2019-11-19 02:16:00 +0600

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -1,70 +1,139 @@
-var w=window, d=document, wiw=w.innerWidth, base=location.protocol+'//'+location.host+location.pathname;
-function gebyid(a,b){return (b||d)["getElementById"](a)}
-function select(a,b){return (b||d)["querySelector"](a)}
-function selall(a,b){return (b||d)["querySelectorAll"](a)}
-function is_elm(a){return a instanceof Element||a instanceof HTMLDocument}
-function is_str(a){return typeof a==='string'||a instanceof String}
-function is_obj(a){return a && typeof a==='object' && a.constructor===Object}
-function is_set(a){return typeof a!=='undefined'}
-function is_arr(a){return Array.isArray(a)}
-function in_arr(a,v){return a.indexOf(v)>-1}
-function append(a,b){return (select(b))['appendChild'](a)}
-function insert(a,b,p='beforeend'){return (select(b))['insertAdjacentHTML'](p,a)}//beforebegin,afterbegin,beforeend,afterend
+/*Remove m=1 from mobile device url*/
+var uri=window.location.href,pos=uri.search(/%3D|%3D%3D|&m=1|\?m=1/);
+if(pos>0)window.history.replaceState({},document.title,uri.substring(0,pos));
 
-function each(a,fn,v){var els=selall(a);for(let i=0;i<els.length;i++)els[i][fn]=v}
-function loop(a,cb){var els=selall(a);for(let i=0;i<els.length;i++)cb(els[i])}
-
-function ajax(url, fn, data=null, method){
-   if(method === void 0) method = data==null?'GET':'POST';
-   var xh = new XMLHttpRequest();
-   xh.open(method, url, true);
-   xh.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-   xh.onreadystatechange = function(){
-      if(this.readyState == 4 && this.status == 200) fn(this.responseText);
-   };
-   xh.send(data);
+var i,prm={},w=window,d=document,wiw=w.innerWidth,wih=w.innerHeight,
+url=w.location.href, baseURL=w.location.protocol+'//'+w.location.host+w.location.pathname;
+function encode(a){return encodeURIComponent(a)}
+function decode(a){return decodeURIComponent(a)}
+function obj2str(o){return JSON.stringify(o)}
+function inarr(a,v){return a.indexOf(v)>=0}
+function isarr(a){return Array.isArray(a)}
+function isset(a){return typeof a !== 'undefined'}
+function iselm(a){return a instanceof Element||a instanceof HTMLDocument}
+function isstr(a){return typeof a==='string'||a instanceof String}
+function isobj(a){return a && typeof a==='object' && a.constructor===Object}
+function ishtml(a){return /<[a-z/][\s\S]*>/i.test(a)}
+function parse(a,t){return new DOMParser().parseFromString(a,t===void 0?'text/html':t)}
+function nocash(url){return url+(url.indexOf('?')>0?'&v=':'?v=')+new Date().getTime()}
+/**
+ * t		string		any html tag name
+ * at		object 		html tag attributes
+ * ht		string		html string
+ * @return	object 		html dom element
+ */
+function elm(t,at,ht){let k,e=d.createElement(t);if(isobj(at))for(k in at)e.setAttribute(k,at[k]);e.innerHTML=ht||at;return e}
+function toelm(a){return elm('div',a).firstChild}
+function select(a,b){if(ishtml(a))a=toelm(a);return iselm(a)?a:(b||d)['querySelector'](a)}
+function selall(a,b){return (b||d)['querySelectorAll'](a)}
+function byid(a,b){return (b||d)['getElementselid'](a)}
+function loop(a,cb){a=isstr(a)?selall(a):(nlist(a)?a:[a]);for(i=0;i<a.length;i++)cb(a[i],i)}
+function each(a,fn,v){a=isstr(a)?selall(a):(nlist(a)?a:[a]);for(i=0;i<a.length;i++)a[i][fn]=v}
+function insert(a,b,p){loop(b, function(e){(e)['insertAdjacentHTML'](p||'beforeend',a)})}/*beforebegin,afterbegin,beforeend,afterend*/
+function append(a,b){(iselm(b)?b:select(b))['appendChild'](iselm(a)?a: select(a))}
+/**
+ * url		string		target url
+ * fn		function 	callback function
+ * d		string		Send data string
+ * m		string		Http Request Method Name
+ * @return	string 		mixed data as string
+ */
+function ajax(url,fn,d,m){
+	var xh=new XMLHttpRequest();
+	xh.open((m||(d?'POST':'GET')),url,true);
+	xh.setRequestHeader('Content-Type','application/x-www-form-urlencoded;charset=UTF-8');
+	xh.onreadystatechange=function(){if(this.readyState==4&&this.status==200)fn(this.responseText)};
+	xh.send(d);
 }
-function elm(tag, attr, html, nl='\n'){
-   var elm = document.createElement(tag);
-   if(is_obj(attr)) for(const[k, v] of Object.entries(attr)) elm.setAttribute(k, v);
-   else if(is_str(attr) && html === void 0) elm.innerHTML = attr;
-   if(html !== void 0) elm.innerHTML = is_arr(html) ? html.join(nl) : html;
-   return elm;
+function url2obj(u){let p,pr={},po=(u||w.location.search).substr(1).split('&');for(i in po){p=po[i].split('=');pr[p[0]]=p[1]} return pr}
+function obj2url(o){let pa;if(isset(o.prm))for(i in o.prm)pa[i]=i+'='+o.prm[i];return pa.join('&')}
+function parseURL(u){
+    let sp, a=elm('a',{href:u||url});
+    return {protocol:a.protocol,
+    			host:a.host,
+    		hostname:a.hostname,
+    			port:a.port,
+    			path:a.pathname,
+    		  search:a.search,
+    		  	pram:url2obj(a.search),
+    		  	hash:a.hash};
 }
-function url(url, attr, each=false){
-   if(!each) url= url+'?v='+new Date().getTime();
-   let ext = url.split(/\#|\?/)[0].split('.').pop().trim();
-   if(ext=='js') return elm('script', attr||{src:url, type:'text/javascript'});
-   else if(ext=='css') return elm('link', attr||{href:url, rel:'stylesheet'});
-   else if(ext=='jpg'||ext=='png'||ext=='gif'||ext=='svg'||ext=='bmp') return elm('img', attr||{src:url});
-   return elm('a', attr||{href:url});
+function eWidth(e){
+	let css=e.currentStyle||w.getComputedStyle(e);
+	return parseFloat(css.width)+parseFloat(css.marginLeft)+parseFloat(css.marginRight)
 }
-function eWidth(elm){
-   var css = elm.currentStyle||window.getComputedStyle(elm);
-   return parseFloat(css.width)+parseFloat(css.marginLeft)+parseFloat(css.marginRight);
-}
-function filter(val, slector, start=true){
-   var els = selall(slector);
-   for(let i = 0; i < els.length; i++) {
-      let txtval = els[i].textContent || els[i].innerText;
-      if(txtval){
-         if(start) els[i].style.display = txtval.toLowerCase().startsWith(val.toLowerCase()) ? '' : 'none';
-         else els[i].style.display = txtval.toLowerCase().indexOf(val.toLowerCase())>-1 ? '' : 'none';
-      }
-   }
+function filter(val, elms, start){
+	loop(elms, function(elm){
+		let txtval = elm.textContent || elm.innerText;
+		if(txtval){
+			if(start||true) elm.style.display=txtval.toLowerCase().startsWith(val.toLowerCase())?'':'none';
+			else elm.style.display=txtval.toLowerCase().indexOf(val.toLowerCase())>-1?'':'none';
+		}
+	})
 }
 function toggle(el,nc){
-   if(el.classList) el.classList.toggle(nc);
-   else{
-      let cls=el.className.split(' '),
-      i=cls.indexOf(nc);
-      el.className = i>=0 ? cls.splice(i, 1) : cls.push(nc).join(" ");
+	loop(el,function(e){
+		if(e.classList) e.classList.toggle(nc);
+		else{
+			let cls=e.className.split(' '),
+			i=cls.indexOf(nc);
+			e.className = i>=0 ? cls.splice(i, 1): cls.push(nc).join(" ");
+		}
+	})
+}
+function storage(name, value){
+	if(name=='clear')localStorage.clear();
+	else if(value === void 0)return localStorage.getItem(name);
+	else if(value=='remove')localStorage.removeItem(name);
+	else localStorage.setItem(name, value);
+}
+/**
+ * listlen	string		any html tag name
+ * page		Integer		current page number 
+ * limit	Integer		limit for each page
+ * sbtn		string		html string
+ * @return	object 		html dom element
+ */
+function paging(listlen, page, limit, sbtn){
+	page = page!==void 0?Number(page):(isset(prm.page)?Number(prm.page):1);
+	limit= limit!==void 0?Number(limit):(isset(prm.limit)?Number(prm.limit):30);
+	sbtn = sbtn!==void 0?Number(sbtn):3;
+	
+	var btns='', _url = url.replace(/&page=\d+/g,''), first, last, start, end, totpag, totbtn, offset, i;
+	if(_url.substr(_url.length-1)=='&') _url = _url.slice(0, -1);
+
+   if(listlen>limit){
+      offset = (page*limit)-limit; 
+      totbtn = (sbtn*2)+1; 
+      totpag = Math.ceil(listlen/limit);
+      for(i=1;i<=totpag;i++){
+         start = page>=totpag-sbtn ? totpag-totbtn : page-sbtn;
+         end   = page<=sbtn?totbtn:page+sbtn;
+         if( i >= start && i <= end){
+            first = (page>=sbtn && totpag>totbtn) ? '<a href="'+_url+'&page='+1+'">first</a>':'';
+            btns += '<a href="'+_url+'&page='+i+'"'+ (i==page? ' class="active"' : '') +'>'+i+'</a>';
+            last = (page<totpag-sbtn)?'<a href="'+_url+'&page='+totpag+'">last</a>':'';
+         }
+      }
+      /*
+      if(totpag>=page) list = list.slice(offset, page<totpag ? offset+limit : list.length);
+      */
+      if(totpag>page)list=offset+' to '+(offset+limit);else if(totpag==page)list=offset+' '+listlen;else list='nill';
+      return list+'<div class="row paging">'+first+btns+last+'</div>';
    }
 }
 /*Responsive position for chield elements inside parent element*/
-function responsive(pelm, celm){
-   var  pw = pelm.offsetWidth, cw = eWidth(celm),
-   nce = Math.floor(pw/cw),
-   rst = Math.floor(pw-nce*cw);
-   select('#dcss').innerHTML = '.'+pelm.className+'{padding:0 '+(rst/2)+'px}';
+function responsive(p){
+	p = select(p); c = selall(c);
+	var pw=p.offsetWidth,cw=eWidth(c[0]),rows=640<pw?5:10,
+	rcnum=Math.floor(pw/cw),
+	rempx=Math.floor(pw-rcnum*cw);
+	if(c.length==1){
+		let boxes = rows*rcnum, boxdata = '';
+		for(let i=0; i<boxes; i++) boxdata += '<div class="box pseudo"></div>';
+		select('.wrapper').innerHTML = boxdata;
+	}
+	p.style.padding = '0 '+rempx/2+'px';
 }
+
+//{auto:2000,stoph:true,res:[[0, 1],[360, 1],[480, 2],[640, 3],[800, 3],[1200, 6]]}
